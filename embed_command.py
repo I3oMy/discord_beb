5e2345ee7b68689b12417913a4635468c53ebfa3
I3oMy <bomcafe130512@gmail.com>
2025-05-15 00:43:19 +0700

    Delete embed_command.py

--- embed_command.py
+++
@@ -1,9 +0,0 @@
-from discord.ext import commands
-
-class EmbedCommand(commands.Cog):
-    def __init__(self, bot):
-        self.bot = bot
-
-    @commands.command()
-    async def hello(self, ctx):
-        await ctx.send("Hello from EmbedCommand!")
